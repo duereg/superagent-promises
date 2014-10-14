9e5440c4c2a7ce85521f19199edd70fee44d0c7c
Matt <duereg@users.noreply.github.com>
2014-10-14 12:57:39 -0700

    Update to use bluebird

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
 'use strict';
 
-var protoEnd,
-    nativePromiseEnd,
-    getPromiseEnd = function(previousEnd) {
-        
-        return function (success, failure) {
-            var self = this,
-                promise = new Promise(function (resolve, reject) {
-                    previousEnd.call(self, function (error, res) {
-                        if (error || !res.ok) {
-                            reject(error || res);
-                        } else {
-                            resolve(res);
-                        }
-                    });
-                });
+var Promise = require('bluebird');
 
-            if (success || failure) {
-                promise = promise.then(success, failure);
+var protoEnd = null,
+  nativePromiseEnd = null,
+  getPromiseEnd = function (previousEnd) {
+    return function (success, failure) {
+      var self = this,
+        promise = new Promise(function (resolve, reject) {
+          previousEnd.call(self, function (error, res) {
+            if (error || !res.ok) {
+              reject(error || res);
+            } else {
+              resolve(res);
             }
-            
-            return promise;
-        };
+          });
+        });
+
+      if (success || failure) {
+        promise = promise.then(success, failure);
+      }
+
+      return promise;
     };
+  };
 
 module.exports = function (request) {
-    protoEnd = protoEnd || request.constructor.prototype.end;
-    request.end = request.end === protoEnd ? (nativePromiseEnd || (nativePromiseEnd = getPromiseEnd(protoEnd))) : getPromiseEnd(request.end);
+  protoEnd = protoEnd || request.constructor.prototype.end;
+  nativePromiseEnd = nativePromiseEnd || getPromiseEnd(protoEnd);
+  request.end = request.end === protoEnd ? nativePromiseEnd : getPromiseEnd(protoEnd);
 };
 
 // useful for testing
 module.exports.uncache = function () {
-    nativePromiseEnd = undefined;
+  nativePromiseEnd = undefined;
 };
